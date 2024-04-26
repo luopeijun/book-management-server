8bb6bcfe5af902749aa5cec82497efe822bb1a43
LuoPeijun <peijun.luo@thoughtworks.com>
2024-04-26 21:27:25 +0800

    feat: init project

---
+++ src/main/java/com/tw/bookmanagementserver/BookManagementServerApplication.java
@@ -0,0 +1,13 @@
+package com.tw.bookmanagementserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookManagementServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookManagementServerApplication.class, args);
+    }
+
+}

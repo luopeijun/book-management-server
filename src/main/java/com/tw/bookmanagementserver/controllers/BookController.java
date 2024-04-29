dcdf2cd26a461b5963c6cdedb8fd0d6c8bda7df2
LuoPeijun <peijun.luo@thoughtworks.com>
2024-04-29 09:15:44 +0800

    feat: add cross origin

--- src/main/java/com/tw/bookmanagementserver/controllers/BookController.java
+++ src/main/java/com/tw/bookmanagementserver/controllers/BookController.java
@@ -4,6 +4,7 @@ import com.tw.bookmanagementserver.models.Book;
 import com.tw.bookmanagementserver.services.BookService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/books")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class BookController {
 
     private final BookService bookService;
